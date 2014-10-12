4c0dd74fc7a98eee00d75d026b6542ab56152d12
Brian Hays <brian.hays@gmail.com>
2014-10-12 17:29:28 +0000

    update enrollments controller

--- app/controllers/enrollments_controller.rb
+++ app/controllers/enrollments_controller.rb
@@ -18,7 +18,7 @@ class EnrollmentsController < ApplicationController
 		      :description => current_course.title,
 		      :currency    => 'usd'
 		    )
-		 end
+		end
 
 	    current_user.enrollments.create(:course => current_course)
 	    redirect_to course_path(current_course)
